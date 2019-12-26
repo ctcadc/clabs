ddbdb40761c90430f1f1a1fb70090f5195a2ff2d
ctcadc <3493946109@qq.com>
2019-12-26 04:34:04 -0800

    added a lab7.c  file

--- lab5.c
+++ lab5.c
@@ -1,32 +1,32 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <string.h>
- 
-void reverse(char s[],int i,int len)
-{
-   int a,b;
- 
-   b = len - (i + 1);
- 
-   if(i < b)
-   {
-        a = s[i];
-        s[i] = s[b];
-        s[b] = a;
- 
-        reverse(s,++i,len);
-   }
+#include<stdio.h>
+int count(char str[]){
+    int i =0;
+    while(str[i]!='\0'){
+        i++;
+    }
+    return i;
 }
- 
+
+void reverse(char a[],char b[]){
+    int c = count(a)-1;
+    int d = 0;
+    while(c>=0){
+        b[d]=a[c];
+        d++;
+        c--;
+    }
+}
+
 int main()
 {
-    char s[100] = "ctcctc";
- 
-    reverse(s,0,strlen(s));
- 
-    printf("%s\n",s);
- 
-    return 0;
- 
+    char a[100];
+    int i=0;
+    a[i]=getchar();
+    while(a[i]!='\n'){
+        i++;
+        a[i]=getchar();
+    }
+    char b[100];
+    reverse(a,b);
+    printf("%s\n",b);
 }
